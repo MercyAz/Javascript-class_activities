58c47bd64c9d77388442d17f020c208d9e3f0ca3
Mercy <116111819+MercyAz@users.noreply.github.com>
2022-11-09 21:08:58 +0000

    initial

---
+++ 02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
@@ -0,0 +1,12 @@
+// WRITE YOUR CODE HERE
+var studentNames = ['martin','lucy' ,'Jordan','Faith','Grace', 'Sam', 'Carol', 'Ellie'];
+
+var totalStudents = [studentNames.length];
+
+var WelcomeQuote = ['Welcome to the class'];
+
+console.log('This class has ' + totalStudents + ' students');
+
+for (let i = 0; i < studentNames.length; i++) {
+   console.log( 'Welcome to the class ' + studentNames[i] );
+  }
