6a233a2650b0537ef7bcde3c02112c6d62ac0b02
Mercy <116111819+MercyAz@users.noreply.github.com>
2022-11-09 22:29:05 +0000

    in_ progress

--- 02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
+++ 02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
@@ -1,12 +1,23 @@
 // WRITE YOUR CODE HERE
-var studentNames = ['martin','lucy' ,'Jordan','Faith','Grace', 'Sam', 'Carol', 'Ellie'];
-
+var studentNames = ['Martin','lucy' ,'Jordan','Faith','Grace', 'Sam', 'Carol', 'Ellie'];
 var totalStudents = [studentNames.length];
-
 var WelcomeQuote = ['Welcome to the class'];
-
+var NewStudent = studentNames[0];
 console.log('This class has ' + totalStudents + ' students');
 
-for (let i = 0; i < studentNames.length; i++) {
-   console.log( 'Welcome to the class ' + studentNames[i] );
-  }
+//for (let i = 1; i < studentNames.length; i++) {
+// console.log( 'Welcome to the class ' + studentNames[i] );
+//  }
+
+console.log(WelcomeQuote + ' ' + studentNames[0])
+console.log(WelcomeQuote + ' ' + studentNames[1])
+console.log(WelcomeQuote + ' ' + studentNames[2])
+console.log(WelcomeQuote + ' ' + studentNames[3])
+console.log(WelcomeQuote + ' ' + studentNames[4])
+console.log(WelcomeQuote + ' ' + studentNames[5])
+console.log(WelcomeQuote + ' ' + studentNames[6])
+console.log(`${WelcomeQuote} ${studentNames[7]}`)
+
+if (NewStudent != studentNames[0])
+console.log('We have a new student');
+else { console.log('We are updated')}
