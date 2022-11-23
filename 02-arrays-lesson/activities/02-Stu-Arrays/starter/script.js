bb7a0aa36c0d19d2d43a69bc69a20c9951099b26
Mercy <116111819+MercyAz@users.noreply.github.com>
2022-11-23 19:18:11 +0000

    solution

--- 02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
+++ 02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
@@ -1,23 +1,60 @@
 // WRITE YOUR CODE HERE
-var studentNames = ['Martin','lucy' ,'Jordan','Faith','Grace', 'Sam', 'Carol', 'Ellie'];
-var totalStudents = [studentNames.length];
-var WelcomeQuote = ['Welcome to the class'];
-var NewStudent = studentNames[0];
-console.log('This class has ' + totalStudents + ' students');
+// var studentNames = ['Martin','lucy' ,'Jordan','Faith','Grace', 'Sam', 'Carol', 'Ellie'];
+// var totalStudents = [studentNames.length];
+// var WelcomeQuote = ['Welcome to the class'];
+// var NewStudent = studentNames[0];
+// console.log('This class has ' + totalStudents + ' students');
 
-//for (let i = 1; i < studentNames.length; i++) {
-// console.log( 'Welcome to the class ' + studentNames[i] );
-//  }
+// //for (let i = 1; i < studentNames.length; i++) {
+// // console.log( 'Welcome to the class ' + studentNames[i] );
+// //  }
 
-console.log(WelcomeQuote + ' ' + studentNames[0])
-console.log(WelcomeQuote + ' ' + studentNames[1])
-console.log(WelcomeQuote + ' ' + studentNames[2])
-console.log(WelcomeQuote + ' ' + studentNames[3])
-console.log(WelcomeQuote + ' ' + studentNames[4])
-console.log(WelcomeQuote + ' ' + studentNames[5])
-console.log(WelcomeQuote + ' ' + studentNames[6])
-console.log(`${WelcomeQuote} ${studentNames[7]}`)
+// console.log(WelcomeQuote + ' ' + studentNames[0])
+// console.log(WelcomeQuote + ' ' + studentNames[1])
+// console.log(WelcomeQuote + ' ' + studentNames[2])
+// console.log(WelcomeQuote + ' ' + studentNames[3])
+// console.log(WelcomeQuote + ' ' + studentNames[4])
+// console.log(WelcomeQuote + ' ' + studentNames[5])
+// console.log(WelcomeQuote + ' ' + studentNames[6])
+// console.log(`${WelcomeQuote} ${studentNames[7]}`)
 
-if (NewStudent != studentNames[0])
-console.log('We have a new student');
-else { console.log('We are updated')}
+// if (NewStudent != studentNames[0])
+// console.log('We have a new student');
+// else { console.log('We are updated')}
+
+var studentsnames = [ 'Micheal', 'Peter', 'Phil', 'Jordan', 'Levis', 'Rhoda'];
+var firstname = studentsnames[0]
+var lastname = studentsnames[5]
+var totalStudents = [studentsnames.length];
+console.log(`This class has ` + totalStudents + ` Students`);
+
+for (i=0; i < totalStudents; i++) {
+    console.log(`Welcome to the class ` + studentsnames[i]);
+}
+
+// studentsnames[0] =('NewStudent')
+studentsnames.push ('NewStudent')
+lastname = 'NewStudent'
+studentsnames[0] = 'AnotherNewStudent'
+firstname = 'AnotherNewStudent'
+console.log(studentsnames)
+
+if(studentsnames[0] == firstname) {
+    console.log( firstname + ' and ' + lastname + ' is in class. ' + 'Welcome to the class ' + firstname + ' and ' +  lastname ) 
+};
+
+
+// var firstname = [studentsnames[0]]
+// If (studentsnames[0] === NewStudent ); {
+//     console.log(`We have a New Student`)
+
+// };
+
+// console.log([2,3,4,5,6][3])
+
+
+// var studentss = ['Gracie', 'Alicia', 'Cryon', 'Beaula']
+// console.log(studentss[studentss.length-1])
+// studentss.pop()
+// studentss.shift()
+// console.log(studentss)
--- 02-arrays-lesson/activities/04-Stu-Iteration/starter/script.js
+++ 02-arrays-lesson/activities/04-Stu-Iteration/starter/script.js
@@ -1,1 +1,9 @@
 //WRITE YOUR CODE BELOW
+
+var students = [ 'Helena', 'Gracia', 'Roselyn', 'Lukas', 'Freddie'];
+
+console.log (students.length);
+
+for (i = 0 ; i < students.length; i ++ ) {
+    console.log(`Great to see you ` + students[i]);
+}
